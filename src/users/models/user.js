020f371eb25e937bad29a26279367bd0475360b7
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-11 14:39:39 -0300

    JS CLASS: user class was added

---
+++ src/users/models/user.js
@@ -0,0 +1,21 @@
+export class User {
+
+    /**
+     * 
+     * @param {Like<User>} userDate 
+     */
+
+    constructor({id, isActive, balance, firstName, lastName, gender}){
+
+        this.id        = id;
+        this.isActive  = isActive;
+        this.balance   = balance;
+        this.avatar    = avatar;
+        this.firstName = firstName;
+        this.lastName  = lastName;
+        this.gender    = gender;
+
+    }
+
+
+}
