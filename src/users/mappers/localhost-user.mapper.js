0e2fdc14bf9f7681f3d9272fe69d8c6fef28a5c0
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-12 15:49:42 -0300

    JS MAPPER: user mapper was added

---
+++ src/users/mappers/localhost-user.mapper.js
@@ -0,0 +1,30 @@
+import { User } from "../models/user";
+
+/**
+ * 
+ * @param {Like<User>} localHostUser 
+ * @returns {User}
+ */
+export const localHostUserToModel = (localHostUser) => {
+
+    const { 
+        avatar,
+        balance,
+        firstName: first_name,
+        gender,
+        id,
+        isActive,
+        lastName: last_name,
+    } = localHostUser
+
+    return new User({
+        avatar,
+        balance,
+        firstName: first_name,
+        gender,
+        id,
+        isActive,
+        lastName: last_name,
+    });
+
+}
