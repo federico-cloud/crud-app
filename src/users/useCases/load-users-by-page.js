b2b4c0b98ca22dd7f0bc8a488513c183e5fdc0f2
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-12 15:50:38 -0300

    JS: use case was modified

--- src/users/useCases/load-users-by-page.js
+++ src/users/useCases/load-users-by-page.js
@@ -1,13 +1,18 @@
+import { localHostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
 
 /**
  * 
  * @param {Number} page
- * @returns
+ * @returns {Promise<User>}
  */
 export const loadUsersByPage = async(page = 1) => {
     const URL = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
     const response = await fetch(URL);
     const data = await response.json();
 
-    console.log(data);
+
+   const users = data.map(localHostUserToModel);
+
+   return users;
 }
