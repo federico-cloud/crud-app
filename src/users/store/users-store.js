77a865c2ef8f7af3ddd8ff9cab7cc0181fb8509f
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-13 11:58:39 -0300

    JS: store was modified

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -7,7 +7,11 @@ const state = {
 
 //TODO
 const loadNextPage = async() => {
-   await loadUsersByPage(state.currentPage + 1);
+    const users = await loadUsersByPage(state.currentPage + 1);
+    if (users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
 }
 
 const loadPreviousPage = async() => {
@@ -30,6 +34,6 @@ export default {
     onUserChanged,
     reloadPage,
 
-    getUser: () => [...state.users],
+    getUsers: () => [...state.users],
     getCurrentPage: () => state.currentPage,
 }
