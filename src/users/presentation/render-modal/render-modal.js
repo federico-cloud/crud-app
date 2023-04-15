cbd85002d6777313b47f964ed23bdf38d2952898
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-15 11:34:45 -0300

    JS PRESENTATION: the listeners in the modal  was added

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -2,6 +2,17 @@ import modalHtml from './render-modal.html?raw';
 import './render-modal.css';
 
 let modal;
+let form;
+
+export const showModal = () =>{
+    modal?.classList.remove('hide-modal');
+};
+
+export const hideModal = () =>{
+    modal?.classList.add('hide-modal');
+    form?.reset();
+}
+
 /**
  * @param {HTMLDivElement}
  */
@@ -11,7 +22,36 @@ export const renderModal = (element) => {
 
     modal = document.createElement('div');
     modal.innerHTML = modalHtml;
-    modal.className = 'modal-container';
+    modal.className = 'modal-container hide-modal';
+
+    form = modal.querySelector('form');
+
+    modal.addEventListener('click', (event) => {
+        if(event.target.className === 'modal-container'){
+            hideModal();
+        }
+    });
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const userData = {};
+
+        for (const [key, value] of formData) {
+            if(key === 'balance'){
+                userData[key] = value * 1;
+                continue;
+            }
+
+            if(key === 'isActive'){
+                userData[key] = value === on ? true : false;
+            }
+
+            userData[key] = value;
+        }
+
+    })
 
     element.append(modal);
 }
