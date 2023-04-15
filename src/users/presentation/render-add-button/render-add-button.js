1742c441433f463114450d39139af4ee00177c88
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-15 11:33:40 -0300

    JS PRESENTATION: the listeners to add button was added

--- src/users/presentation/render-add-button/render-add-button.js
+++ src/users/presentation/render-add-button/render-add-button.js
@@ -1,3 +1,4 @@
+import { showModal } from '../render-modal/render-modal';
 import './render-add-button.css';
 
 /**
@@ -13,7 +14,7 @@ export const renderAddButton = (element) => {
     element.append(fabButton);
 
     fabButton.addEventListener('click', () => {
-        throw Error('not implemented')
+        showModal();
     });
 
 }
