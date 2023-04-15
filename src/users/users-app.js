da0208e50b0fbf062c2f39f21343adf3e7053897
Federico Galan <fegv.2@hotmail.com.ar>
2023-04-15 10:59:50 -0300

    JS: renderModal and renderAddButton was implemented

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,8 @@
-import { renderButtons } from './presentation/render-buttons/render-buttons';
-import { renderTable } from './presentation/render-table/render-table';
-import usersStore from './store/users-store';
+import { renderAddButton }  from './presentation/render-add-button/render-add-button';
+import { renderButtons }    from './presentation/render-buttons/render-buttons';
+import { renderModal }      from './presentation/render-modal/render-modal';
+import { renderTable }      from './presentation/render-table/render-table';
+import usersStore           from './store/users-store';
 
 /**
  * 
@@ -14,5 +16,6 @@ export const UserApp = async(element) => {
 
     renderTable(element);
     renderButtons(element);
-
+    renderAddButton(element);
+    renderModal(element);
 }
